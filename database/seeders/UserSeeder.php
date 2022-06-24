a287333f82dd4b729f5ee57c06eb5e546f0efb24
Galang Ekayudha Permana <galange99@gmail.com>
2022-06-24 10:28:21 +0700

    feat: add seeder admin & bumdes

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,15 +11,6 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
-        $bumdes = Bumdes::create([
-            'name' => 'Desa Sukamaju',
-            'phone' => '081234567890',
-            'district_id' => 337410,
-            'address' => 'Sendangmulyo',
-            'postal_code' => '50272',
-            'description' => 'Sekedar BUMDes biasa',
-        ]);
-        
         User::create([
             'name' => 'Administrator',
             'email' => 'admin@polines.ac.id',
@@ -31,6 +22,31 @@ class UserSeeder extends Seeder
             'district_id' => 337410,
             'gender_id' => 1,
             'role_id' => 1,
+            'user_status_id' => 1,
+            'verified' => true,
+        ]);
+
+        $bumdes = Bumdes::create([
+            'name' => 'Desa Sukamaju',
+            'phone' => '081234567890',
+            'district_id' => 337410,
+            'address' => 'Sendangmulyo',
+            'postal_code' => '50272',
+            'description' => 'Sekedar BUMDes biasa',
+        ]);
+
+        // Bumdes User
+        User::create([
+            'name' => 'Galang',
+            'email' => 'galang@polines.ac.id',
+            'password' => Hash::make('galang'),
+            'phone' => '089656544023',
+            'balance' => 0,
+            'address' => 'Perum Griya Tria Indah 2 no.27 Sendangmulyo',
+            'postal_code' => '50272',
+            'district_id' => 337410,
+            'gender_id' => 1,
+            'role_id' => 2,
             'user_status_id' => 1,
             'bumdes_id' => $bumdes->id,
             'verified' => true,
