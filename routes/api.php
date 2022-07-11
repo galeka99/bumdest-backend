cde425bbf9987a5a92a95f29504fa3be5fc01013
Galang Ekayudha Permana <galange99@gmail.com>
2022-07-11 14:32:35 +0700

    .

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\LocationController;
 use App\Http\Controllers\ProjectApiController;
 use App\Http\Controllers\RatingApiController;
 use App\Http\Controllers\UserApiController;
+use App\Http\Controllers\WithdrawApiController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function() {
